b46fb7b992ab6a5ead82eb40ffa50ec212b1d4b5
rmfls0606 <rmfls0606@naver.com>
2024-04-18 15:14:12 +0900

    입력부분 생성

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -8,35 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text: String = "3"
     var body: some View {
-        VStack{
+        VStack(){
+            //입력부분 생성
+            Text(text)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, maxHeight: 200, alignment: .bottomTrailing)
+                .padding()
+                
+            
             //키패드 생성
             Grid(horizontalSpacing: 10, verticalSpacing: 10) {
                 GridRow {
-                    KeypadBtn(pad: "7")
-                    KeypadBtn(pad: "8")
-                    KeypadBtn(pad: "9")
-                    KeypadBtn(pad: "/")
+                    KeypadBtn(pad: "7", text: $text)
+                    KeypadBtn(pad: "8", text: $text)
+                    KeypadBtn(pad: "9", text: $text)
+                    KeypadBtn(pad: "/", text: $text)
                 }//: GRIDROW
                 GridRow {
-                    KeypadBtn(pad: "4")
-                    KeypadBtn(pad: "5")
-                    KeypadBtn(pad: "6")
-                    KeypadBtn(pad: "*")
+                    KeypadBtn(pad: "4", text: $text)
+                    KeypadBtn(pad: "5", text: $text)
+                    KeypadBtn(pad: "6", text: $text)
+                    KeypadBtn(pad: "*", text: $text)
                 }//: GRIDROW
                 GridRow {
-                    KeypadBtn(pad: "1")
-                    KeypadBtn(pad: "2")
-                    KeypadBtn(pad: "3")
-                    KeypadBtn(pad: "-")
+                    KeypadBtn(pad: "1", text: $text)
+                    KeypadBtn(pad: "2", text: $text)
+                    KeypadBtn(pad: "3", text: $text)
+                    KeypadBtn(pad: "-", text: $text)
                 }//: GRIDROW
                 GridRow {
-                    KeypadBtn(pad: ".")
-                    KeypadBtn(pad: "0")
-                    KeypadBtn(pad: "C")
-                    KeypadBtn(pad: "+")
+                    KeypadBtn(pad: ".", text: $text)
+                    KeypadBtn(pad: "0", text: $text)
+                    KeypadBtn(pad: "C", text: $text)
+                    KeypadBtn(pad: "+", text: $text)
                 }//: GRIDROW
-                KeypadBtn(pad: "=")
+                KeypadBtn(pad: "=", text: $text)
             }//: GRID
             .padding(.vertical, 10)
             .background(.white)
@@ -48,9 +57,17 @@ struct ContentView: View {
 //Keypad Button View Component
 struct KeypadBtn: View {
     let pad: String
+    @Binding var text: String
     var body: some View {
         Button(action: {
-            
+            switch pad{
+            case "=":
+                calculate()
+            case "C":
+                text = ""
+            default:
+                text += pad
+            }
         }, label: {
             Rectangle()
                 .stroke(.black,lineWidth: 1)
@@ -61,6 +78,10 @@ struct KeypadBtn: View {
                         .frame(width: .infinity, height: .infinity)
                 }
         })
+    }
+    
+    func calculate(){
+        
     }
 }
 
