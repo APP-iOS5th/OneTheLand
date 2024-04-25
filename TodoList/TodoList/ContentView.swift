56332780c18e63b6bd0895cadac8154b6d9d438b
Seunghye Hwang <ferrydraw@naver.com>
2024-04-25 14:02:57 +0900

    Initial TodoList

---
+++ TodoList/TodoList/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by 황승혜 on 4/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 황승혜 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
